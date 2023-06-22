4f2f44d252202edb373e97fed0a10261d4e059d0
Priyankagcse <136603375+Priyankagcse@users.noreply.github.com>
2023-06-22 13:42:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
-const mysql = require('mysql');
-const {v4: uuidv4} = require('uuid');
-const DBHost = '127.0.0.1';
-const DBUser = 'root';
-const DBPassword = 'Welcome@123';
-const DBName = 'testing';
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const app = express();
+// const mysql = require('mysql');
+// const {v4: uuidv4} = require('uuid');
+// const DBHost = '127.0.0.1';
+// const DBUser = 'root';
+// const DBPassword = 'Welcome@123';
+// const DBName = 'testing';
 
-const db = mysql.createPool({
-    connectionLimit: 10,
-    host: DBHost,
-    user: DBUser,
-    password: DBPassword,
-    database: DBName
-});
+// const db = mysql.createPool({
+//     connectionLimit: 10,
+//     host: DBHost,
+//     user: DBUser,
+//     password: DBPassword,
+//     database: DBName
+// });
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
-module.exports = { app, uuidv4, db, DBHost, DBUser, DBPassword, DBName };
+// module.exports = { app, uuidv4, db, DBHost, DBUser, DBPassword, DBName };
