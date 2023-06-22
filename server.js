8a65159c18138765f70376863638cb349fb84685
Priyankagcse <136603375+Priyankagcse@users.noreply.github.com>
2023-06-22 13:44:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,18 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.setHeader("Access.Control.Allow.Credentials", "true");
+    // res.setHeader("Access.Control.Allow.Credentials", "true");
     res.send('Welcome');
+});
+
+app.get("/priya", (req, res) => {
+    // res.setHeader("Access.Control.Allow.Credentials", "true");
+    res.send('priya');
+});
+
+app.get("/priyanka", (req, res) => {
+    // res.setHeader("Access.Control.Allow.Credentials", "true");
+    res.send('priyanka');
 });
 
 app.listen(5000, () => {
