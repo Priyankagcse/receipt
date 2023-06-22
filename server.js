ad0e439bb2a19a6a593e75ead1455029746cf714
Priyankagcse <136603375+Priyankagcse@users.noreply.github.com>
2023-06-22 13:43:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
+    res.setHeader("Access.Control.Allow.Credentials", "true");
     res.send('Welcome');
 });
 
