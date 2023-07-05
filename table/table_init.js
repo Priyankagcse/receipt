c9efa16c07db2115bcfa4ad955473288ba0d21c0
Priyankagcse <priyankagcse16@gmail.com>
2023-07-05 17:07:42 +0530

    TABLE UPDATE

--- table/table_init.js
+++ table/table_init.js
@@ -10,55 +10,51 @@ class table_refresh {
         this.refresh = async(res = null, callBack) => {
             try {
                 for (let table of this.tables) {
-                    // console.log(`(SELECT EXISTS (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA LIKE '${DBName}' AND TABLE_NAME = '${table.name}'))`);
                     const sqlInsert = await this.connection.connection_query(`SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA LIKE '${DBName}' AND TABLE_NAME = '${table.name}'`);
                     console.log(sqlInsert.length);
                     if (sqlInsert.length) {
-                        console.log(sqlInsert[0][0]['ALTER']);
-                        if (sqlInsert[0][0]['ALTER'] === "ALTER") {
-                            console.log(`SHOW COLUMNS FROM ${table.name}`);
-                            const sqlColumns = await this.connection.connection_query(`SHOW COLUMNS FROM ${table.name}`);
-                            if (sqlColumns.length) {
-                                let fieldListStr = '';
-                                let modifyListStr = '';
-                                for (var j = 0; j < table.queryList.length; j++) {
-                                    let filterData = sqlColumns.filter((cols) => cols.Field === table.queryList[j]['field']);
-                                    if (filterData.length === 0) {
-                                        fieldListStr += `${table.queryList[j]['field']} ${table.queryList[j]['type']} ${table.queryList[j]['null']},`;
-                                    } else {
-                                        if (filterData[0]['Type'] !== table.queryList[j]['type']) {
-                                            modifyListStr += `MODIFY COLUMN ${table.queryList[j]['field']} ${table.queryList[j]['type']},`;
-                                        }
-                                    }
-                                }
-                                fieldListStr = fieldListStr.replace(/.$/,"");
-                                modifyListStr = modifyListStr.replace(/.$/,"");
-                                if (fieldListStr) {
-                                    console.log(`ALTER TABLE ${table.name} ADD (${fieldListStr})`);
-                                    await this.connection.connection_query(`ALTER TABLE ${table.name} ADD (${fieldListStr})`);
-                                    if (modifyListStr) {
-                                        console.log(`ALTER TABLE ${table.name} ${modifyListStr}`);
-                                        await this.connection.connection_query(`ALTER TABLE ${table.name} ${modifyListStr}`);
-                                    }
+                        console.log(`SHOW COLUMNS FROM ${table.name}`);
+                        const sqlColumns = await this.connection.connection_query(`SHOW COLUMNS FROM ${table.name}`);
+                        if (sqlColumns.length) {
+                            let fieldListStr = '';
+                            let modifyListStr = '';
+                            for (var j = 0; j < table.queryList.length; j++) {
+                                let filterData = sqlColumns.filter((cols) => cols.Field === table.queryList[j]['field']);
+                                if (filterData.length === 0) {
+                                    fieldListStr += `${table.queryList[j]['field']} ${table.queryList[j]['type']} ${table.queryList[j]['null']},`;
                                 } else {
-                                    if (modifyListStr) {
-                                        console.log(`ALTER TABLE ${table.name} ${modifyListStr}`);
-                                        await this.connection.connection_query(`ALTER TABLE ${table.name} ${modifyListStr}`);
+                                    if (filterData[0]['Type'] !== table.queryList[j]['type']) {
+                                        modifyListStr += `MODIFY COLUMN ${table.queryList[j]['field']} ${table.queryList[j]['type']},`;
                                     }
                                 }
                             }
-                        } else {
-                            let fieldList = table.queryList.map((line) => {
-                                return `${line.field} ${line.type} ${line.null}`;
-                            });
-                            let fieldListStr = fieldList.join(',')
-                            let queryFormat = fieldListStr;
-                            if (table.query) {
-                                queryFormat += `, ${table.query}`;
+                            fieldListStr = fieldListStr.replace(/.$/,"");
+                            modifyListStr = modifyListStr.replace(/.$/,"");
+                            if (fieldListStr) {
+                                console.log(`ALTER TABLE ${table.name} ADD (${fieldListStr})`);
+                                await this.connection.connection_query(`ALTER TABLE ${table.name} ADD (${fieldListStr})`);
+                                if (modifyListStr) {
+                                    console.log(`ALTER TABLE ${table.name} ${modifyListStr}`);
+                                    await this.connection.connection_query(`ALTER TABLE ${table.name} ${modifyListStr}`);
+                                }
+                            } else {
+                                if (modifyListStr) {
+                                    console.log(`ALTER TABLE ${table.name} ${modifyListStr}`);
+                                    await this.connection.connection_query(`ALTER TABLE ${table.name} ${modifyListStr}`);
+                                }
                             }
-                            console.log(`CREATE TABLE ${table.name} (${queryFormat});`);
-                            await this.connection.connection_query(`CREATE TABLE ${table.name} (${queryFormat});`);
                         }
+                    } else {
+                        let fieldList = table.queryList.map((line) => {
+                            return `${line.field} ${line.type} ${line.null}`;
+                        });
+                        let fieldListStr = fieldList.join(',')
+                        let queryFormat = fieldListStr;
+                        if (table.query) {
+                            queryFormat += `, ${table.query}`;
+                        }
+                        console.log(`CREATE TABLE ${table.name} (${queryFormat});`);
+                        await this.connection.connection_query(`CREATE TABLE ${table.name} (${queryFormat});`);
                     }
                 }
             } catch(err) {
